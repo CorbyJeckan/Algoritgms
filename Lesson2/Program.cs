c6dcd4e909b35195bf06df7a655ff04f75800cd4
Евгений Савельев <curtisjeckan@mail.ru>
2022-08-30 14:05:15 +0500

    выполнил дз ко 2 уроку

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+
+namespace Thread2447455
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            DoublyLinkedList<string> linkedList = new DoublyLinkedList<string>();
+            // добавление элементов
+            linkedList.Add("Bob");
+            linkedList.Add("Bill");
+            linkedList.Add("Tom");
+            linkedList.AddFirst("Kate");
+
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+
+            linkedList.Remove("Bill");
+
+            // перебор с последнего элемента
+            foreach (var t in linkedList.BackEnumerator())
+            {
+                Console.WriteLine(t);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
